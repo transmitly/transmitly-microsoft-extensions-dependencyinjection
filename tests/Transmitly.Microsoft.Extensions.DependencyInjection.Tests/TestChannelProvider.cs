cafa70fa975fadf7ae58f8c80d8e9b1a1f819ef9
jeremy <517429+JeremyMelton@users.noreply.github.com>
2024-03-12 22:19:48 -0600

    fix: build failure

--- tests/Transmitly.Microsoft.Extensions.DependencyInjection.Tests/TestChannelProvider.cs
+++ tests/Transmitly.Microsoft.Extensions.DependencyInjection.Tests/TestChannelProvider.cs
@@ -2,40 +2,20 @@
 
 namespace Transmitly.Microsoft.Extensions.DependencyInjection.Tests
 {
-	class TestChannelProvider : IChannelProviderClient<object>, IChannelProviderClient<IEmail>
+	class TestChannelProvider(SimpleDependency foo) : IChannelProviderClient<object>, IChannelProviderClient<IEmail>
 	{
-		public readonly SimpleDependency _foo;
+		public readonly SimpleDependency _foo = foo ?? throw new ArgumentNullException(nameof(foo));
 
-		public TestChannelProvider(SimpleDependency foo)
-		{
-			_foo = foo ?? throw new ArgumentNullException(nameof(foo));
-		}
 		public IReadOnlyCollection<string>? RegisteredEvents => throw new NotImplementedException();
 
 		public Task<IReadOnlyCollection<IDispatchResult?>> DispatchAsync(object communication, IDispatchCommunicationContext communicationContext, CancellationToken cancellationToken)
 		{
-			return Task.FromResult<IReadOnlyCollection<IDispatchResult?>>([new DispatchResult(true, "Object")]);
+			return Task.FromResult<IReadOnlyCollection<IDispatchResult?>>([new DispatchResult(DispatchStatus.Dispatched, "Object")]);
 		}
 
 		public Task<IReadOnlyCollection<IDispatchResult?>> DispatchAsync(IEmail communication, IDispatchCommunicationContext communicationContext, CancellationToken cancellationToken)
 		{
-			return Task.FromResult<IReadOnlyCollection<IDispatchResult?>>([new DispatchResult(true, "IEmail")]);
-		}
-	}
-
-	class TestChannelProvider2 : IChannelProviderClient<object>
-	{
-		public readonly SimpleDependency _foo;
-
-		public TestChannelProvider2(SimpleDependency foo)
-		{
-			_foo = foo ?? throw new ArgumentNullException(nameof(foo));
-		}
-		public IReadOnlyCollection<string>? RegisteredEvents => throw new NotImplementedException();
-
-		public Task<IReadOnlyCollection<IDispatchResult?>> DispatchAsync(object communication, IDispatchCommunicationContext communicationContext, CancellationToken cancellationToken)
-		{
-			return Task.FromResult<IReadOnlyCollection<IDispatchResult?>>([new DispatchResult(true, "Object2")]);
+			return Task.FromResult<IReadOnlyCollection<IDispatchResult?>>([new DispatchResult(DispatchStatus.Dispatched, "IEmail")]);
 		}
 	}
 }
