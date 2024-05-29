54a0d30d37e9889e566c5196f1ab107866334a7a
jeremy <517429+JeremyMelton@users.noreply.github.com>
2024-05-28 21:42:35 -0600

    update naming for platform identity concept

--- src/Transmitly.Microsoft.Extensions.DependencyInjection/EmptyClient.cs
+++ src/Transmitly.Microsoft.Extensions.DependencyInjection/EmptyClient.cs
@@ -7,52 +7,56 @@ using Transmitly.Verification;
 
 namespace Transmitly.Microsoft.Extensions.DependencyInjection
 {
-	sealed class EmptyClient : ICommunicationsClient
-	{
-		public void DeliverReport(DeliveryReport report)
-		{
-			throw new NotImplementedException();
-		}
+    sealed class EmptyClient : ICommunicationsClient
+    {
+        public void DeliverReport(DeliveryReport report)
+        {
+            throw new NotImplementedException();
+        }
 
-		public void DeliverReports(IReadOnlyCollection<DeliveryReport> reports)
-		{
-			throw new NotImplementedException();
-		}
+        public void DeliverReports(IReadOnlyCollection<DeliveryReport> reports)
+        {
+            throw new NotImplementedException();
+        }
 
-		public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IAudience> audiences, IContentModel contentModel, IReadOnlyCollection<string> allowedChannels, string? cultureInfo = null, CancellationToken cancellationToken = default)
-		{
-			throw new NotImplementedException();
-		}
+        public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IPlatformIdentity> platformIdentities, ITransactionModel transactionalModel, IReadOnlyCollection<string> allowedChannels, string? cultureInfo = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
 
-		public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IAudienceAddress> audienceAddresses, IContentModel contentModel, IReadOnlyCollection<string> allowedChannels, string? cultureInfo = null, CancellationToken cancellationToken = default)
-		{
-			throw new NotImplementedException();
-		}
+        public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IIdentityAddress> identityAddresses, ITransactionModel transactionalModel, IReadOnlyCollection<string> allowedChannels, string? cultureInfo = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
 
-		public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, string audienceAddress, IContentModel contentModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
-		{
-			throw new NotImplementedException();
-		}
+        public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, string identityAddress, ITransactionModel transactionalModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
 
-		public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, string audienceAddress, object contentModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
-		{
-			throw new NotImplementedException();
-		}
+        public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, string identityAddress, object transactionalModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
 
-		public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<string> audienceAddresses, object contentModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
-		{
-			throw new NotImplementedException();
-		}
+        public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<string> identityAddresses, object transactionalModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
 
-		public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IAudienceAddress> audienceAddresses, IContentModel contentModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
-		{
-			throw new NotImplementedException();
-		}
+        public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IIdentityAddress> identityAddresses, ITransactionModel transactionalModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
 
-		public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IAudience> audiences, IContentModel contentModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
-		{
-			throw new NotImplementedException();
-		}
+        public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IPlatformIdentity> platformIdentities, ITransactionModel transactionalModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
 
-	}
+        public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IIdentityReference> identityReferences, ITransactionModel transactionalModel, string? cultureInfo = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
