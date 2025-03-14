92dddd17728c8cdc51e82011ff970b1dc26901b0
Jeremy <517429+JeremyMelton@users.noreply.github.com>
2025-03-13 22:30:53 -0600

    feat: builder configurator & custom client (#4)
    * fix: add guards against null params
    
    * add ability to use a configurator to configure the builder
    add ability to use a configurator and custom communications client

---
+++ tests/Transmitly.Microsoft.Extensions.DependencyInjection.Tests/InjectedClient.cs
@@ -0,0 +1,65 @@
+﻿// ﻿﻿Copyright (c) Code Impressions, LLC. All Rights Reserved.
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License")
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using Transmitly;
+using Transmitly.Delivery;
+
+namespace Microsoft.Extensions.DependencyInjection.Tests
+{
+	public partial class TransmitlyDependencyInjectionExtensionsTests
+	{
+		class InjectedClient : ICommunicationsClient
+		{
+			private readonly Lazy<ICommunicationsClient> _lazy;
+
+			public InjectedClient(ICommunicationsClientConfigurator configurator)
+			{
+				_lazy = new Lazy<ICommunicationsClient>(() =>
+				{
+					var cfg = new CommunicationsClientBuilder();
+					configurator.ConfigureClient(cfg);
+					return cfg.BuildClient();
+				});
+			}
+
+			public ICommunicationsClient Client
+			{
+				get
+				{
+					return _lazy.Value;
+				}
+			}
+			public void DeliverReport(DeliveryReport report)
+			{
+				Client.DeliverReport(report);
+			}
+
+			public void DeliverReports(IReadOnlyCollection<DeliveryReport> reports)
+			{
+				Client.DeliverReports(reports);
+			}
+
+			public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IPlatformIdentityProfile> platformIdentities, ITransactionModel transactionalModel, IReadOnlyCollection<string> channelPreferences, string? cultureInfo = null, CancellationToken cancellationToken = default)
+			{
+				return Client.DispatchAsync(pipelineName, platformIdentities, transactionalModel, channelPreferences, cultureInfo, cancellationToken);
+			}
+
+			public Task<IDispatchCommunicationResult> DispatchAsync(string pipelineName, IReadOnlyCollection<IPlatformIdentityReference> identityReferences, ITransactionModel transactionalModel, IReadOnlyCollection<string> channelPreferences, string? cultureInfo = null, CancellationToken cancellationToken = default)
+			{
+				return Client.DispatchAsync(pipelineName, identityReferences, transactionalModel, channelPreferences, cultureInfo, cancellationToken);
+			}
+		}
+
+	}
+}
---
+++ tests/Transmitly.Microsoft.Extensions.DependencyInjection.Tests/TransmitlyDependencyInjectionExtensionsTests.cs
@@ -0,0 +1,152 @@
+﻿// ﻿﻿Copyright (c) Code Impressions, LLC. All Rights Reserved.
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License")
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//      http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+using Moq;
+using Transmitly;
+
+namespace Microsoft.Extensions.DependencyInjection.Tests
+{
+	[TestClass()]
+	public partial class TransmitlyDependencyInjectionExtensionsTests
+	{
+		[TestMethod]
+		public void AddTransmitlyShouldCallConfigureClientOnResolution()
+		{
+			var services = new ServiceCollection();
+			var tracker = new ConfigureClientTracker();
+
+			services.AddSingleton(tracker);
+			services.AddTransmitly(typeof(MockClientConfigurator));
+
+			var provider = services.BuildServiceProvider();
+
+			var client = provider.GetRequiredService<ICommunicationsClient>();
+			client.DeliverReports([]);
+
+			Assert.IsTrue(tracker.WasCalled, "ConfigureClient was not called on MockClientConfigurator.");
+		}
+
+		[TestMethod]
+		public void AddTransmitlyShouldCallConfigureClientOnResolution2()
+		{
+			// Arrange
+			var services = new ServiceCollection();
+			var configuratorMock = new Mock<ICommunicationsClientConfigurator>();
+			configuratorMock
+			.Setup(x => x.ConfigureClient(It.IsAny<CommunicationsClientBuilder>()))
+			.Verifiable();
+
+			services.AddSingleton(configuratorMock.Object);
+			services.AddTransmitly<ICommunicationsClientConfigurator>();
+
+			var provider = services.BuildServiceProvider();
+
+			var client = provider.GetRequiredService<ICommunicationsClient>();
+			client.DeliverReports([]);
+			// Assert - Verify ConfigureClient was called exactly once
+			configuratorMock.Verify(c => c.ConfigureClient(It.IsAny<CommunicationsClientBuilder>()), Times.Once);
+		}
+
+		[TestMethod]
+		public void AddTransmitlyGenericShouldCallConfigureClientOnResolution()
+		{
+			var services = new ServiceCollection();
+			var tracker = new ConfigureClientTracker();
+
+			services.AddSingleton(tracker);
+			services.AddTransmitly<MockClientConfigurator>();
+
+			var provider = services.BuildServiceProvider();
+
+			// Act - Trigger service resolution
+			var client = provider.GetRequiredService<ICommunicationsClient>();
+			client.DeliverReports([]);
+			// Assert - Ensure ConfigureClient was called
+			Assert.IsTrue(tracker.WasCalled, "ConfigureClient was not called on MockClientConfigurator.");
+		}
+
+		[TestMethod]
+		public async Task InjectedClientShouldBeRegisteredAsync()
+		{
+			var configuratorMock = new Mock<ICommunicationsClientConfigurator>();
+			configuratorMock.Setup(x => x.ConfigureClient(It.IsAny<CommunicationsClientBuilder>())).Verifiable();
+
+			var services = new ServiceCollection();
+			services.AddSingleton(configuratorMock.Object);
+			services.AddTransmitly<ICommunicationsClientConfigurator, InjectedClient>();
+			//services.AddSingleton(configuratorMock.Object);
+
+			var provider = services.BuildServiceProvider();
+			var client = provider.GetService<ICommunicationsClient>();
+
+			Assert.IsNotNull(client);
+			Assert.IsInstanceOfType<InjectedClient>(client);
+
+		}
+
+		[TestMethod]
+		public async Task InjectedClientShouldNotCallConfigureClientMultipleTimes()
+		{
+
+			var configuratorMock = new Mock<ICommunicationsClientConfigurator>();
+			configuratorMock
+			.Setup(x => x.ConfigureClient(It.IsAny<CommunicationsClientBuilder>()))
+			.Callback<CommunicationsClientBuilder>(builder =>
+			{
+				builder.AddPipeline("testPipeline", pipeline => { });
+			})
+			.Verifiable();
+
+			var services = new ServiceCollection();
+			services.AddSingleton(configuratorMock.Object);
+			services.AddTransmitly<ICommunicationsClientConfigurator, InjectedClient>();
+			//services.AddSingleton(configuratorMock.Object);
+
+			var provider = services.BuildServiceProvider();
+			var client = provider.GetRequiredService<ICommunicationsClient>();
+
+			await client.DispatchAsync("testPipeline", new List<IPlatformIdentityProfile>(), TransactionModel.Create(new { }), new List<string>());
+			await client.DispatchAsync("testPipeline", new List<IPlatformIdentityProfile>(), TransactionModel.Create(new { }), new List<string>());
+
+			configuratorMock.Verify(x => x.ConfigureClient(It.IsAny<CommunicationsClientBuilder>()), Times.Once);
+		}
+
+		[TestMethod]
+		public async Task DefaultClientShouldNotCallConfigureClientMultipleTimes()
+		{
+
+			var configuratorMock = new Mock<ICommunicationsClientConfigurator>();
+			configuratorMock
+			.Setup(x => x.ConfigureClient(It.IsAny<CommunicationsClientBuilder>()))
+			.Callback<CommunicationsClientBuilder>(builder =>
+			{
+				builder.AddPipeline("testPipeline", pipeline => { });
+			})
+			.Verifiable();
+
+			var services = new ServiceCollection();
+			services.AddSingleton(configuratorMock.Object);
+			services.AddTransmitly<ICommunicationsClientConfigurator>();
+			//services.AddSingleton(configuratorMock.Object);
+
+			var provider = services.BuildServiceProvider();
+			var client = provider.GetRequiredService<ICommunicationsClient>();
+
+			await client.DispatchAsync("testPipeline", new List<IPlatformIdentityProfile>(), TransactionModel.Create(new { }), new List<string>());
+			await client.DispatchAsync("testPipeline", new List<IPlatformIdentityProfile>(), TransactionModel.Create(new { }), new List<string>());
+
+			configuratorMock.Verify(x => x.ConfigureClient(It.IsAny<CommunicationsClientBuilder>()), Times.Once);
+		}
+	}
+}
